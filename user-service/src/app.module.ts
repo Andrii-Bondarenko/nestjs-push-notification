76aef4f3f9b5a3fa195e86a14bf4710973a6817e
Andrii Bondarenko <andrii_bondarenko2@epam.com>
2025-07-31 20:10:31 +0000

    chore(user-service): initialize user-service with NestJS framework, TypeORM, PostgreSQL

---
+++ user-service/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './shared/database/database.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule
+  ]
+})
+export class AppModule {}
